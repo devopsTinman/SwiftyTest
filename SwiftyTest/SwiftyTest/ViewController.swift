7a418b0bd7ae784a49c4d1cfc7b3056261747178
DevOps Tinman <devops.tinman@gmail.com>
2019-12-16 23:22:09 -0600

    Initial Commit

---
+++ SwiftyTest/SwiftyTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyTest
+//
+//  Created by Allbee, Eamon on 12/16/19.
+//  Copyright © 2019 Allbee, Eamon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
